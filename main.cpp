979b6ccf18af829c4628b4b8574c515f7d0933ed
WillHayes096283 <wilthaye@ut.utm.edu>
2022-08-25 11:36:19 -0500

    4th commit: Added sum to main.cpp

--- main.cpp
+++ main.cpp
@@ -3,8 +3,29 @@
 
 using namespace std;
 
+int sum (int n);
+
 int main() {
+  int n = 0;
+
+  int temp = 0;
+  
   cout << "Hello World!" << endl;
+
+  cout << "Enter an integer: ";
+
+  cin >> n;
+
+  temp = sum(n);
+
+  cout << "Sum of integer: " << temp << endl;
   
   return 0;
+}
+
+int sum (int n) {
+  int temp = 1 + n;
+
+  return temp;
+  
 }
