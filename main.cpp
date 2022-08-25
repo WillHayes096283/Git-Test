cd3fda78bfa38ca7159d37ae6a50c9ce3b47d44e
WillHayes096283 <wilthaye@ut.utm.edu>
2022-08-25 12:26:13 -0500

    added documentation to main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,39 @@
+/**
+ * @file main.cpp
+ * @author William Hayes
+ * @date 2022-08-25
+ * @brief This program allows the user to enter an integer and it prints out
+ * the integer's factorial sum and product.
+ * 
+ * The take an integer as a input and prints out the factorial sum and product
+ * of said integer.
+ */
+
 
 #include <iostream>
 
 using namespace std;
 
+
+/**
+ * This function calculates the factorial sum for some integer
+ *
+ * @param int n the integer the user wants to get the factorial sum of
+ * @pre n has to be an integer greater than 0
+ * @return int the factorial sum of n
+ * 
+ */
 int sum (int n);
 
+
+/**
+ * This function calculates the factorial product some integer
+ *
+ * @param int n the integer the user wants to get the factorial product of
+ * @pre n has to be an integer greater than 0
+ * @return int the factorial product of n
+ * 
+ */
 int prod (int n);
 
 int main() {
